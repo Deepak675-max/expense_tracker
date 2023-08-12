e5306b75d3ce569db3976c52bc27e440bf938b61
deepak kumar <deepak.kumar@nourishgenie.in>
2023-08-12 12:14:47 +0530

    added async/await task and updated posts app

--- promises.js
+++ promises.js
@@ -4,52 +4,42 @@ const user = {
     lastAcitivityTime: new Date().getTime()
 }
 
-function createPost(post) {
-    return new Promise((resolve, reject) => {
-        try {
-            posts.push(post);
-            resolve(user.lastAcitivityTime);
-        } catch (error) {
-            reject(error);
-        }
-
-    })
-}
-
-function updateLastActivityTime() {
-    return new Promise((resolve, reject) => {
-        setTimeout(function () {
-            try {
-                user.lastAcitivityTime = new Date().getTime();
-                resolve(user.lastAcitivityTime);
-            } catch (error) {
-                reject(error);
-            }
-        }, 1000)
-    })
-}
-
-function deletePost() {
-    return new Promise((resolve, reject) => {
-        try {
-            posts.pop();
-            resolve(posts);
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
-
-
-Promise.all([createPost({ title: "post two", body: "this is my second post" }), updateLastActivityTime()])
-    .then(([createPostResolve, updateLastActivityTimeResolve]) => {
-        console.log(`Before creating ${posts[posts.length - 1].title}, user last activity time  =  ${createPostResolve}`);
+async function createPost(post) {
+    try {
+        posts.push(post);
+        console.log(`Before creating ${posts[posts.length - 1].title}, user last activity time  =  ${user.lastAcitivityTime}`);
         console.log(`after creating ${posts[posts.length - 1].title} >>>>>>>>`)
         console.log("posts >>>", posts);
-        console.log("user last acitivity time ", user.lastAcitivityTime);
-    })
-    .then(deletePost)
-    .then((deletePostResolve) => {
-        console.log("After deleting last post, the posts >>> ", deletePostResolve);
-    })
-    .catch(error => console.log("Error ", error));
+    } catch (error) {
+        console.log(error);
+        throw (error);
+    }
+}
+
+async function updateLastActivityTime() {
+    setTimeout(function () {
+        try {
+            user.lastAcitivityTime = new Date().getTime();
+            console.log("user last acitivity time ", user.lastAcitivityTime);
+        } catch (error) {
+            console.log(error);
+            throw (error);
+        }
+    }, 1000)
+}
+
+async function deletePost() {
+    try {
+        posts.pop();
+        console.log("After deleting last post, the posts >>> ", posts);
+    } catch (error) {
+        console.log(error);
+        throw (error);
+    }
+}
+
+async function resolvePromises() {
+    await Promise.all([createPost({ title: "post two", body: "this is my second post" }), updateLastActivityTime(), deletePost()]);
+}
+
+resolvePromises();
