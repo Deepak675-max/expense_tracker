dc48f319b4381cd8e5176e9cc23ef3414e60c8f4
deepak kumar <deepak.kumar@nourishgenie.in>
2023-09-29 11:32:22 +0530

    updated code

--- leaderboard.js
+++ leaderboard.js
@@ -2,10 +2,35 @@
 const axoisInstance = axios.create({
     baseURL: 'http://52.66.114.142:3000/api'
 })
+const token = localStorage.getItem('token');
+
+
+async function logoutUser() {
+    try {
+        if (!token) {
+            throw new Error('unauthorized user');
+        }
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        }
+        console.log(config)
+        const responseData = await axoisInstance.get('/auth/logout', config);
+        console.log(responseData);
+        if (responseData.data.error) {
+            throw responseData.data.error
+        }
+        return responseData.data.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
 
 async function getLeaderboardData() {
     try {
-        const token = localStorage.getItem('token');
         if (!token) {
             throw new Error('unauthorized user');
         }
@@ -66,7 +91,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
             loadLeaderborad(leaderboardData);
         })
         .catch(error => {
-            this.window.location.href = 'table.html'
+            this.window.location.href = 'index.html'
         })
 });
 
@@ -80,3 +105,15 @@ function loadLeaderborad(leaderboardData) {
     document.getElementById('leaderboard-box').style.display = 'block';
 
 }
+
+document.getElementById('logout-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    logoutUser()
+        .then(responseData => {
+            localStorage.clear('token');
+            window.location.href = 'login.html';
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
